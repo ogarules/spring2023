443375f92bf187e8f8c9178c57cc22ff1ecacd93
Oscar <oscar@digitalchallengers.com.mx>
2023-09-02 09:06:11 -0600

    Ejercicio2 parte 1

---
+++ Core/src/main/java/com/example/demo/ejercicio2/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.example.demo.ejercicio2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorld {
+    private String mensaje;
+
+    // public HelloWorld(){
+
+    // }
+
+    // public HelloWorld(String mensaje){
+    //     this.mensaje=mensaje;
+    // }
+
+    // public String getMensaje() {
+    //     return mensaje;
+    // }
+
+    // public void setMensaje(String mensaje) {
+    //     this.mensaje = mensaje;
+    // }
+}
